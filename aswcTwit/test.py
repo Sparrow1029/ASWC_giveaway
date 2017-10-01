c3f8e79478d291442afefec551534552a3786537
Sparrow1029 <arayart88@gmail.com>
2017-10-01 00:55:10 -0400

    Ahhh.. I'm learning. Here's the first tiny script (making sure it works). OAuth Tokens are not saved in this repo (since it's public).

---
+++ aswcTwit/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import twitter
+
+api = twitter.Api(consumer_key='',
+                  consumer_secret='',
+                  access_token_key='',
+                  access_token_secret='')
+# IT'S ALIVE!
+# status = api.PostUpdate('Test using python-twitter...')
+# print(status.text)
